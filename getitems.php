986f6899ec5cc2a6ffcf8d39228581ea5beeb2f9
amirulasri <amirulasrix@gmail.com>
2022-03-29 20:47:22 +0800

    Update

--- getitems.php
+++ getitems.php
@@ -1,16 +1,49 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:8100');
-include('conn.php');
-$orderid = 1;
-$increment = 1;
-$responsedata = array();
-try {
-    $statementgetitems = $conn->prepare("SELECT * FROM items WHERE custid = ?");
-    $statementgetitems->execute([$orderid]);
-    while ($row = $statementgetitems->fetch(PDO::FETCH_OBJ)) {
-        $responsedata[] = $row;
-    }
-} catch (PDOException $e) {
-    //echo $e->getMessage();
+include_once 'conn.php';
+include_once 'config/cors.php';
+include_once 'vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$orderid = "";
+if(isset($_GET['d'])){
+    $orderid = $_GET['d'];
 }
-echo json_encode($responsedata);
+$authHeader = getallheaders();
+if (isset($authHeader['Authorization']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+    $token = $authHeader['Authorization'];
+    $token = explode(" ", $token)[1];
+    try {
+        $key = "CVs3gubehD35BjiiSFs8Ie7Xt0YX2OITufeqdxvAb20QW497j8GA7DGV3VmWjU8";
+        $decoded = JWT::decode($token, new Key($key, 'HS256'));
+        $arraydecoded = (array)$decoded;
+
+        http_response_code(200);
+
+        //CHECK IF USER EXISTS IN DATA
+        $stmt = $conn->prepare("SELECT * FROM customerlogin WHERE username=?");
+        $stmt->execute([$arraydecoded['username']]);
+        if ($stmt->rowCount() > 0) {
+            $userdata = $stmt->fetch();
+            if ($arraydecoded['id'] == $userdata['id']) {
+                $stmt = $conn->prepare("SELECT * FROM items WHERE orderid=?");
+                $stmt->execute([$orderid]);
+                while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+                    $responsedata[] = $row;
+                }
+                echo json_encode($responsedata);
+            } else {
+                echo json_encode(array('status' => 'error'));
+            }
+        } else {
+            echo json_encode(array('status' => 'error'));
+        }
+    } catch (Exception $e) {
+        http_response_code(401);
+        echo json_encode(array('status' => 'error'));
+    }
+} else {
+    http_response_code(401);
+    echo json_encode(array('status' => 'error'));
+}
